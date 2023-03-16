99638f9c0bb48774eba9ce4babdc43c81010fd8d
Mati-X <88286186+Mati-X@users.noreply.github.com>
2023-03-16 07:44:23 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 
 # /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -14,8 +15,34 @@ class Todo(db.Model):
     title = db.Column(db.String(50))
     description = db.Column(db.String(100))
     is_done = db.Column(db.Boolean)
-    date = db.Column(db.DateTime)
+    date = db.Column(db.String(19))
 
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    description = request.form.get("description")
+    now = datetime.datetime.now()
+    date_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    new_todo = Todo(title=title, description=description, is_done=False, date=date_string)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.is_done = not todo.is_done
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 @app.route("/")
 def index():
